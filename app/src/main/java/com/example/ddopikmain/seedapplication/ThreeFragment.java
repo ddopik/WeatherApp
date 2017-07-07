d6c00c02dca30c34bd5d656e1118e18f87076a92
ddopik <ddopik.01@gmail.com>
2017-07-07 14:16:00 +0200

    intializing Task

---
+++ app/src/main/java/com/example/ddopikmain/seedapplication/ThreeFragment.java
@@ -0,0 +1,27 @@
+package com.example.ddopikmain.seedapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ddopikMain on 2/20/2017.
+ */
+
+public class ThreeFragment extends android.support.v4.app.Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/ddopikmain/seedapplication/TwoFragment.java
@@ -0,0 +1,29 @@
+package com.example.ddopikmain.seedapplication;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ddopikMain on 2/20/2017.
+ */
+
+public class TwoFragment extends android.support.v4.app.Fragment{
+
+    public TwoFragment(){};
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
